6f569b14816805373628aa1c2ba2a9bb760127e5
Sailesh Patel <saileshapatel@gmail.com>
2022-02-11 20:19:04 +0000

    Create a List component
    Holds all of the items in the list

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 'use strict';
 
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <ul>
+                <li>Example Item</li>
+            </ul>
+        )
+    }
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -8,9 +23,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                Hello world
-            </div>
+            <List />
         );
     }
 }
